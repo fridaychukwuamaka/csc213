0d690b4b666334c99e4ad10cfafc0db01d38cf70
fridaychukwuamaka <38787543+fridaychukwuamaka@users.noreply.github.com>
2018-04-30 14:06:11 +0100

    Update assignment2018.asm

--- assignment2018.asm
+++ assignment2018.asm
@@ -1,21 +1,20 @@
 # File: assignment2018.asm
-#Author: Friday chukwuamaka
+#Author: Friday chukwuamaka Kareen
 #MAT NO.: COS/1663/2016
 #level: 200lv
-#purpose a program to compute the root of a quadratic equation
-
+#Purpose: A PROGRAM TO COMPUTE THE ROOT AO A QUADRATIC EQUATION 
 
 .data
-prompt:.asciiz "\nEnter the value of a\n"
-prompt1:.asciiz "Enter the value of b\n"
-prompt2:.asciiz "Enter the value of c\n"
+prompt:.asciiz "\nEnter the value of a:\n"
+prompt1:.asciiz "Enter the value of b:\n"
+prompt2:.asciiz "Enter the value of c:\n"
 msg1:.asciiz "Complex number\n"
 num:.float 4.0
 num1:.float 2.0
-msg2:.asciiz "The roots of the valus inputed are:\n"
+msg2:.asciiz "The roots of the values inputed are:\n"
 msg3:.asciiz " and "
 msg4:.asciiz "The purpose of this  program is to compute the root of a quadratic equation"
-msg5:.asciiz "\n \n"
+msg5:.asciiz "\n "
 
 .text
 main:
@@ -44,7 +43,7 @@ li $v0 6
 syscall
 mov.s $f1 $f0
 
-#assk user for the value of b
+#ask user for the value of b
 li $v0 4
 la $a0 prompt1
 syscall
@@ -62,29 +61,6 @@ li $v0 6
 syscall
 mov.s $f3 $f0
 
-jal calculation
-
-li $v0 4
-la $a0 msg2
-syscall
-
-li $v0 2
-mov.s     $f12 $f11 
-syscall
-
-li $v0 4
-la $a0 msg3
-syscall
-
-li $v0 2
-mov.s    $f12 $f13 
-syscall
-
-
-b exit
-	
-calculation:
-
 # a*c
 mul.s   $f30 $f1 $f3
 #4*a*c
@@ -95,20 +71,43 @@ mul.s $f28 $f2 $f2
 sub.s $f27 $f28 $f29
 
 mfc1 $t2 $f27
-bltz   $t2 msg
+#if  $t2 <0
+bltz   $t2 else
 
 #sqrt of d 
 sqrt.s  $f26 $f27
 
 #make $f2(b) negative
 neg.s  $f9 $f2
+
 add.s $f25 $f9 $f26
 sub.s $f22 $f9 $f26
 mul.s $f8 $f5 $f1
 div.s  $f11 $f25 $f8
 div.s  $f13 $f22 $f8
-jr $ra
-msg:
+
+#first result
+li $v0 4
+la $a0 msg2
+syscall
+
+li $v0 2
+mov.s     $f12 $f11 
+syscall
+
+
+li $v0 4
+la $a0 msg3
+syscall
+
+#second result
+li $v0 2
+mov.s    $f12 $f13 
+syscall
+b exit
+
+#else
+else:
 li $v0 4
 la $a0 msg1
 syscall
